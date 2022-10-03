17971e008317744864e1646b0869ccdf49e7a71b
BgMg <brandong.manzog@gmail.com>
2022-10-03 01:53:05 -0600

    enable sql

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,41 @@
-#[macro_use]
 extern crate diesel;
 
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 use dotenvy::dotenv;
+use std::env;
+
+use diesel::pg::PgConnection;
+use diesel::prelude::*;
+
+use diesel::r2d2::Pool;
+use diesel::r2d2::{self, ConnectionManager};
+
+pub mod models;
+pub mod schema;
+
+pub type DbPool = r2d2::Pool<ConnectionManager<PgConnection>>;
 
 #[get("/")]
-async fn hello_world() -> impl Responder {
-    format!("Hello world!")
+async fn index(pool: web::Data<DbPool>) -> impl Responder {
+    let conn = pool.get().expect("It doesn't connect to SQL");
+    match web::block(move || {posts.load::<Post>(&conn)}) {
+        
+    }
+    return format!("Hello world!");
 }
 
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(hello_world))
+    dotenv().ok();
+    let database_url = env::var("DATABASE_URL").expect("DB url variable doesn't found");
+
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+
+    let pool = Pool::builder()
+        .build(manager)
+        .expect("No se pudo construir la pool");
+
+    HttpServer::new(move || App::new().service(index).app_data(pool.clone()))
         .bind(("localhost", 8080))?
         .run()
         .await
