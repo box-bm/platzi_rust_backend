9d5db93f0321b77cf2b6be7b5937c74fde055441
BgMg <brandong.manzog@gmail.com>
2022-10-02 20:19:25 -0600

    creating api

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
+#[macro_use]
 extern crate diesel;
 
-pub mod models;
-pub mod schema;
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use dotenvy::dotenv;
 
-fn main() {
-    
+#[get("/")]
+async fn hello_world() -> impl Responder {
+    format!("Hello world!")
+}
+
+#[actix_web::main] // or #[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(hello_world))
+        .bind(("localhost", 8080))?
+        .run()
+        .await
 }
