113a802237212e1cf0c2631b3976306ef679f629
BgMg <brandong.manzog@gmail.com>
2022-10-04 00:34:01 -0600

    end actix

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ extern crate diesel;
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 use dotenvy::dotenv;
 use std::env;
-use std::fmt::format;
 
 use diesel::pg::PgConnection;
 use diesel::prelude::*;
@@ -16,7 +15,7 @@ pub mod schema;
 
 pub type DbPool = r2d2::Pool<ConnectionManager<PgConnection>>;
 
-use self::models::{Post, NewPost, NewPostHandler};
+use self::models::{NewPostHandler, Post};
 use self::schema::posts::dsl::*;
 
 #[get("/")]
@@ -32,14 +31,13 @@ async fn index(pool: web::Data<DbPool>) -> impl Responder {
 #[post["/new_post"]]
 async fn insert(pool: web::Data<DbPool>, item: web::Json<NewPostHandler>) -> impl Responder {
     let mut conn = pool.get().expect("It doesn't connect to SQL");
-
-    let new_post = NewPost {
-        title: item.title.clone().as_str(),
-        slug: "fouth-post",
-        body: item.body.as_str()
+    
+    let new_post = NewPostHandler {
+        title: item.title.clone(),
+        body: item.body.clone(),
     };
 
-    return match web::block(move || {Post::create_post(conn, new_post)}).await {
+    return match web::block(move || Post::create_post(&mut conn, &new_post)).await {
         Ok(data) => HttpResponse::Ok().body(format!("{:?}", data.ok())),
         Err(err) => HttpResponse::Ok().body("hubo un error"),
     };
--- src/models.rs
+++ src/models.rs
@@ -1,5 +1,6 @@
+use super::schema::posts;
 use diesel::prelude::*;
-use diesel::{Insertable, Queryable, PgConnection, RunQueryDsl};
+use diesel::{Insertable, PgConnection, Queryable};
 use serde::{Deserialize, Serialize};
 
 #[derive(Queryable, Debug, Deserialize, Serialize)]
@@ -17,12 +18,10 @@ pub struct Post {
 }
 
 #[derive(Clone, Serialize, Deserialize, Debug)]
-pub struct NewPostHandler  {
-    pub title : String,
-    pub body: String
+pub struct NewPostHandler {
+    pub title: String,
+    pub body: String,
 }
-
-use crate::schema::posts;
 
 #[derive(Insertable)]
 #[diesel(table_name = posts)]
@@ -32,19 +31,24 @@ pub struct NewPost<'a> {
     pub slug: &'a str,
 }
 
-
 impl Post {
-
-    pub fn slugify (title: &String) -> String {
-        return  title.replace(" ", "-").to_lowercase();
+    pub fn slugify(title: &String) -> String {
+        return title.replace(" ", "-").to_lowercase();
     }
-    pub fn create_post<'a>(conn: &PgConnection, post: &NewPostHandler) -> Result<Post, diesel::result::Error> {
+    pub fn create_post<'a>(
+        conn: &mut PgConnection,
+        post: &NewPostHandler,
+    ) -> Result<Post, diesel::result::Error> {
         let slug = Post::slugify(&post.title.clone());
+
         let new_post = NewPost {
             title: &post.title,
             slug: &slug,
-            body: &post.body
+            body: &post.body,
         };
-        return diesel::insert_into(posts::table).values(new_post).get_result::<Post>(&mut conn);
+
+        diesel::insert_into(posts::table)
+            .values(new_post)
+            .get_result::<Post>(conn)
     }
 }
